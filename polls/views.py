b1a2127a841200d9faa3eb1518afb04e7087fabe
ajrvs <rxaykz1@gmail.com>
2024-03-08 12:46:48 +0530

    Updated view (for each poll)

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,9 @@ def home(request):
     polls = Poll.objects.all()
     context = {"polls":polls}
     return render(request, "polls/home.html", context)
+
+def poll(request, id):
+    poll = Poll.objects.filter(pk=id)
+    choices = Poll.objects.filter(poll=poll)
+    context = {"poll":poll, "choices":choices}
+    return render(request, "polls/poll.html", context)
