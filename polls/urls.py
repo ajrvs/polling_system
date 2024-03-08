0e5ec4d571ef95a38127fa4bb87216df4146035b
ajrvs <rxaykz1@gmail.com>
2024-03-08 18:52:20 +0530

    Updated all the views

--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("polls/", views.home, name="home"), # not necessary
+    path("polls/all/", views.all, name="all"),
+    path("polls/<int:id>", views.poll, name="poll"),
+    path("polls/<int:id>/vote", views.vote, name="vote"),
+    path("polls/<int:id>/detail", views.detail, name="detail"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from .models import *
 
 # Create your views here.
@@ -8,7 +9,36 @@ def home(request):
     return render(request, "polls/home.html", context)
 
 def poll(request, id):
-    poll = Poll.objects.filter(pk=id)
-    choices = Poll.objects.filter(poll=poll)
+    poll = get_object_or_404(Poll, pk=id)
+    choices = Choice.objects.filter(poll=poll)
     context = {"poll":poll, "choices":choices}
     return render(request, "polls/poll.html", context)
+
+def vote(request, id):
+    poll = Poll.objects.get(pk=id)
+    choices = Choice.objects.filter(poll=poll)
+    if request.method == "POST":
+        selected_choice_id = request.POST.get("choice")
+        # selected_choice = Choice.objects.get(pk=selected_choice_id) -> raise exception that needs to be manally handled
+        if selected_choice_id:
+            selected_choice = get_object_or_404(Choice, pk=selected_choice_id)
+            selected_choice.votes += 1
+            selected_choice.save()
+            messages.success(request, "Voted successfully!")
+            return redirect("detail", id=id)
+        else:
+            messages.error(request, "Please select a choice before voting.")
+        context = {"poll":poll, "choices":choices}
+        return render(request, "polls/poll.html", context)
+
+def detail(request, id):
+    poll = get_object_or_404(Poll, pk=id)
+    choices = Choice.objects.filter(poll=poll)
+    context = {"poll":poll, "choices":choices}
+    return render(request, "polls/detail.html", context)
+
+def all(request):
+    polls = Poll.objects.all()
+    # choices = Choice.objects.filter(poll=poll)
+    context = {"polls":polls}
+    return render(request, "polls/all.html", context)
